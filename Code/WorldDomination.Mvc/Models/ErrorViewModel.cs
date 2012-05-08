de39913d5d4c206cba20ba0f086e3612f9db59be
Pure Krome <pure.krome@world-domination.com.au>
2012-05-09 00:16:19 +1000

    - Can now set the Location custom Header. - Can pass in a ModelState into the ErrorViewModel.

--- Code/WorldDomination.Mvc/Models/ErrorViewModel.cs
+++ Code/WorldDomination.Mvc/Models/ErrorViewModel.cs
@@ -1,4 +1,9 @@
-﻿using System.Net;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web.Mvc;
 
 namespace WorldDomination.Web.Mvc.Models
 {
@@ -6,6 +11,36 @@ namespace WorldDomination.Web.Mvc.Models
 
     public class ErrorViewModel : BaseApiViewModel
     {
+        public ErrorViewModel()
+        {
+        }
+
+        public ErrorViewModel(IDictionary<string, ModelState> modelStateDictionary)
+        {
+            if (modelStateDictionary == null)
+            {
+                throw new ArgumentNullException("modelStateDictionary");
+            }
+
+            if (modelStateDictionary.Count <= 0)
+            {
+                return;
+            }
+
+            var errorMessage = new StringBuilder();
+            foreach (ModelError error in modelStateDictionary.Values.SelectMany(x => x.Errors))
+            {
+                if (errorMessage.Length > 0)
+                {
+                    errorMessage.Append(" ");
+                }
+
+                errorMessage.Append(error.ErrorMessage);
+            }
+
+            ErrorMessage = errorMessage.ToString();
+        }
+
         public HttpStatusCode ErrorStatus { get; set; }
         public string ErrorMessage { get; set; }
     }
---
+++ Code/WorldDomination.Mvc/WebMvcExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Web;
+
+namespace WorldDomination.Web.Mvc
+{
+    public static class WebMvcExtensions
+    {
+        public static void Location(this HttpResponseBase value, HttpRequestBase requestBase, string path)
+        {
+            if (requestBase == null || requestBase.Url == null)
+            {
+                throw new ArgumentNullException("requestBase");
+            }
+
+            var urlBuilder = new UriBuilder(requestBase.Url.AbsoluteUri)
+                             {
+                                 Path = path
+                             };
+
+            value.AddHeader("Location", urlBuilder.Uri.AbsoluteUri);
+        }
+    }
+}
